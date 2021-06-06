21326d17a94ed9d3ea9b7d7018788ecb273f1acd
javierffs <fjfernan@PMINTL.NET>
2021-06-06 14:11:47 -0300

    a_02

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const { send } = require('process');
+const app = express();
+const pathPublic = (path.resolve(__dirname, './public'));
+app.use(express.static(pathPublic));
+app.listen(3000,() => {
+    console.log("Servidor escuchando en puerto 3000")
+}
+);
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+}
+);
+
