c13171649a42ebb8c225af4abb13b75d658a1a0a
javierffs <fjfernan@PMINTL.NET>
2021-06-08 21:50:23 -0300

    modifique sarasa

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const { send } = require('process');
 const app = express();
 const pathPublic = (path.resolve(__dirname, './public'));
 app.use(express.static(pathPublic));
-app.listen(3000,() => {
+/*app.listen(3000,() => {
+
     console.log("Servidor escuchando en puerto 3000")
 }
-);
+); */
 
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 app.get('/home', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 }
